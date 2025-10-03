560da50734c33d90cbbe67578cd3698079f81041
Taha Furkan Teke <tahafurkan.39@gmail.com>
2025-10-03 16:12:19 +0300

    Refactor Swagger setup in Program.cs
    Moved Swagger setup to always execute after building the app.

--- ExcelDataManagementAPI/Program.cs
+++ ExcelDataManagementAPI/Program.cs
@@ -78,26 +78,26 @@ namespace ExcelDataManagementAPI
                 });
             });
 
-            var app = builder.Build();
+                    var app = builder.Build();
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Excel Data Management API v1");
+                c.RoutePrefix = "swagger";
+            });
 
             if (app.Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Excel Data Management API v1");
-                    c.RoutePrefix = "swagger";
-                });
-                
                 app.UseCors("DevelopmentPolicy");
-
             }
             else
             {
                 app.UseHttpsRedirection();
                 app.UseCors("ApiPolicy");
             }
+
             app.UseStaticFiles();
             app.UseAuthorization();
             app.MapControllers();
